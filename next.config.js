7be210c5ac8918105551f9860815b4ac8fdcde05
Tim O'Connell <tim.oconnell.australia@gmail.com>
2023-10-19 10:20:39 +1100

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+const path = require("path");
+const { withPayload } = require("@payloadcms/next-payload");
 
-module.exports = nextConfig
+module.exports = withPayload(
+  {
+    experimental: {
+      serverComponentsExternalPackages: ["pino"],
+    },
+  },
+  {
+    // The second argument to `withPayload`
+    // allows you to specify paths to your Payload dependencies
+    // and configure the admin route to your Payload CMS.
+
+    // Point to your Payload config (Required)
+    configPath: path.resolve(__dirname, "./src/payload/payload.config.ts"),
+
+    // Point to custom Payload CSS (optional)
+    //cssPath: path.resolve(__dirname, "./css/my-custom-payload-styles.css"),
+
+    // Point to your exported, initialized Payload instance (optional, default shown below`)
+    payloadPath: path.resolve(process.cwd(), "./src/payload/payloadClient.ts"),
+
+    // Set a custom Payload admin route (optional, default is `/admin`)
+    // NOTE: Read the "Set a custom admin route" section in the payload/next-payload README.
+    adminRoute: "/admin",
+  }
+);
